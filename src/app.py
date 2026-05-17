a4251c6c4852e079f430d9c065908612719f190b
Marco Sorich <marco@sorich.de>
2026-05-17 21:10:43 +0000

    Enable dash's dev tools via DASH_DEBUG=true environment variable.

--- src/app.py
+++ src/app.py
@@ -47,6 +47,7 @@ else:
 
 # Create the Dash app
 app = dash.Dash(__name__, meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}])
+app.enable_dev_tools(debug=os.getenv("DASH_DEBUG", "false").lower() == "true")
 
 log.debug(f'Initialization time: {(time.time() - start_time)*1000:,.2f}ms')
 
